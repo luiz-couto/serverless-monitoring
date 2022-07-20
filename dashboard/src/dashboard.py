5eed7b92599713d92acf518fa01df07edb0ac97b
luiz-couto <luiz.felipe.556@outlook.com>
2022-07-20 18:08:34 -0300

    update dashboard

--- dashboard/src/dashboard.py
+++ dashboard/src/dashboard.py
@@ -2,8 +2,9 @@ import redis
 import json
 import numpy as np
 import dash_bootstrap_components as dbc
-from dash import Dash, html, dcc
 import plotly.graph_objects as graph
+
+from dash import Dash, html, dcc
 from plotly.subplots import make_subplots
 from dash.dependencies import Input, Output
 
@@ -16,11 +17,6 @@ redis_client = redis.Redis(host='192.168.121.189', port=6379)
 app.min = []
 app.hour = [] 
 app.vmem = []
-
-def get_redis_data():
-    app.cpu_data = json.loads(redis_client.get('luizcouto-proj3-output'))
-
-get_redis_data()
 
 app.layout = html.Div([
     html.Div([
@@ -41,8 +37,6 @@ app.layout = html.Div([
 ])
 
 
-
-# https://dash.plotly.com/basic-callbacks
 @app.callback(Output('cpus_min', 'figure'), Input('interval0', 'n_intervals'))
 def build_cpu_dashboard(n):
     data = json.loads(redis_client.get('luizcouto-proj3-output'))
@@ -67,7 +61,13 @@ def build_cpu_dashboard(n):
     )
     
     for i in range(len(curr_records)):
-        figs.add_trace(graph.Scatter(x=np.arange(len(app.min)*len(curr_records)), y=np.array(app.min)[:,i], name="cpu%d_60sec"%i, mode="lines", line={'color':'rgb(%d,%d,%d)'%((i*73)%255, (i*25) % 255,(i*11) % 255)}), row=1, col=1)
+        figs.add_trace(graph.Scatter(x=np.arange(len(app.min)*len(curr_records)), 
+                                    y=np.array(app.min)[:,i], 
+                                    name="CPU " + str(i), 
+                                    mode="lines",
+                                    line={'color':'rgb(%d,%d,%d)'%((i*73)%255, (i*25) % 255,(i*11) % 255)}),
+                                    row=1,
+                                    col=1)
     
     
     figs.update_xaxes(title_text = 'Time')
@@ -105,7 +105,13 @@ def build_cpu_dashboard(n):
     )
     
     for i in range(len(curr_records)):
-        figs.add_trace(graph.Scatter(x=np.arange(len(app.hour)*len(curr_records)), y=np.array(app.hour)[:,i], name="cpu%d_60sec"%i, mode="lines", line={'color':'rgb(%d,%d,%d)'%((i*73)%255, (i*25) % 255,(i*11) % 255)}), row=1, col=1)
+        figs.add_trace(graph.Scatter(x=np.arange(len(app.hour)*len(curr_records)),
+                                    y=np.array(app.hour)[:,i], 
+                                    name="CPU " + str(i),
+                                    mode="lines",
+                                    line={'color':'rgb(%d,%d,%d)'%((i*73)%255, (i*25) % 255,(i*11) % 255)}),
+                                    row=1,
+                                    col=1 )
     
     
     figs.update_xaxes(title_text = 'Time')
@@ -119,14 +125,13 @@ def build_cpu_dashboard(n):
     return figs
 
 
-# https://dash.plotly.com/basic-callbacks
 @app.callback(Output('memory-percent', 'figure'), Input('interval1', 'n_intervals'))
 def build_vmem_dashboard(n):
-    get_redis_data()
-    app.vmem.append(app.cpu_data['mvg_avg_memory_last_min'])
+    data = json.loads(redis_client.get('luizcouto-proj3-output'))
+    app.vmem.append(data['mvg_avg_memory_last_min'])
 
-    if len(app.vmem) > 300:
-        app.vmem = app.vmem[-300:]
+    if len(app.vmem) > NUMBER_RECORDS:
+        app.vmem = app.vmem[-NUMBER_RECORDS:]
     
     figs = make_subplots(
         rows = 1,
@@ -134,10 +139,16 @@ def build_vmem_dashboard(n):
         subplot_titles = ("")
     )
 
-    figs.add_trace(graph.Scatter(x=np.arange(len(app.vmem)), y=np.array(app.vmem), name="virtual_memory_60min", mode="lines", line={'color':'rgb(255,255,0)'}), row=1, col=1)
+    figs.add_trace(graph.Scatter(x=np.arange(len(app.vmem)),
+                                y=np.array(app.vmem),
+                                name="memory_percent_last_min",
+                                mode="lines",
+                                line={'color':'rgb(255,255,0)'}),
+                                row=1,
+                                col=1 )
 
     figs.update_xaxes(title_text = 'Time (s)')
-    figs.update_yaxes(title_text = 'Utilization (Megabytes)')
+    figs.update_yaxes(title_text = 'Utilization (%)')
 
     figs.update_layout(
         width = 1200,
